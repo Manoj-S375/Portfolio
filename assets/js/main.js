1c5c8fe100923a7c9c77fa1654fae37c43cd3e33
Manoj.S-375 <manoj12a2004@gmail.com>
2026-04-29 23:27:50 +0530

    Zoho Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Python Coder","Web Developer", "Java Enthusiast"],
+  strings : ["Java Coder","Backend Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
